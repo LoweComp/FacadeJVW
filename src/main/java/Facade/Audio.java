82759429f3b05e200ccb212326902e7adcb7e63f
LoweComp <terci_300@outlook.com>
2025-12-09 12:44:16 -0300

    Sistemas e Façade que Executa

---
+++ src/main/java/Facade/Audio.java
@@ -0,0 +1,10 @@
+package Facade;
+
+public class Audio {
+    public String ligarAmplificadores() {
+        return "Áudio: Amplificadores ligados.";
+    }
+    public String calibrarVolume() {
+        return "Áudio: Volume calibrado para 7.1 Surround.";
+    }
+}
